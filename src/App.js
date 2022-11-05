c3424243d1fc8c932600bbcbcd703ffbabee4faa
Ellie Dove <edove1@umbc.edu>
2022-11-05 19:18:19 -0400

    add chatbox ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef, createRef } from "react";
+import { io } from "socket.io-client";
+import "./App.css";
+
+const socket = io("http://localhost:3030");
+
+socket.on("connect", () => {});
 
 function App() {
+  const [messages, setMessages] = useState(["hello", "wut"]);
+
+  const [currMessage, setCurrMessage] = useState("");
+  const messagesEndRef = createRef();
+
+  const handleMessageChange = (event) => {
+    setCurrMessage(event.target.value);
+  };
+
+  const handleSendClick = (event) => {
+    sendMessage();
+  };
+
+  const sendMessage = () => {
+    setMessages([...messages, currMessage]);
+    // need to send message to socket here
+    setCurrMessage("");
+  };
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const handleSendEnter = (event) => {
+    if (event.key === "Enter" || event.code === "NumpadEnter") {
+      sendMessage();
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid grid-columns-10 gap-4">
+        <div className="bg-gray-200 absolute inset-y-24 right-10 w-1/4 max-w-sm rounded shadow-lg overflow-scroll">
+          <ul className="text-left ">
+            {messages.map((msg, i) => (
+              <li className="ml-2" key={i}>
+                {msg}
+              </li>
+            ))}
+          </ul>
+          <div ref={messagesEndRef}></div>
+          <div className="bg-gray-200 relative bottom-0 max-w-sm rounded shadow-lg">
+            <input
+              className="fixed bottom-14 right-24"
+              onChange={handleMessageChange}
+              value={currMessage}
+              onKeyDown={handleSendEnter}
+            ></input>
+            <button
+              className="fixed bottom-14 right-12 bg-blue-500 hover:bg-blue-700 text-white py-1 rounded"
+              onClick={handleSendClick}
+            >
+              send
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
